22fe7ecf7254b825f8d31b866c6aef6da279994b
Kkyyss <ongkangyi@gmail.com>
2018-10-27 03:22:13 +0800

    Update

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import json
 import os
+import time
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_restful import Resource, Api
 from flask_mail import Mail, Message
 from flask_cors import CORS
 
 from flask_jwt_extended import (
     JWTManager, jwt_required, create_access_token,
-    decode_token
+    decode_token, get_jwt_identity
 )
 
 
@@ -31,7 +32,13 @@ api = Api(app)
 
 
 class EmailNotification(Resource):
+    @jwt_required
     def post(self):
+        current_user = get_jwt_identity()
+
+        if not current_user:
+            return None, 401
+
         if request.data:
             req = request.get_json(force=True)
             emails = req['emails']
@@ -52,16 +59,17 @@ class EmailNotification(Resource):
                     try:
                         mail.send(msg)
                     except:
-                        return None
+                        return None, 400
+                    return None, 200
 
 
 class EmailSendToken(Resource):
     def post(self):
         if request.data:
             req = request.get_json(force=True)
-            if req['email']:
-                token = create_access_token(identity=req['email'], expires_delta=False)
-                link = "http://localhost:3000/register/" + req['email'] + "/" + token
+            if req['email'] and req['role']:
+                token = create_access_token(identity={ 'email': req['email'], 'role': req['role'] })
+                link = "http://localhost:3000/login/" + req['email'] + "/" + req['role'] + "/" + token
                 with mail.connect() as conn:
                     msg = Message(
                         subject="[Blockchain Online Voting] Registration Verification",
@@ -77,26 +85,33 @@ class EmailSendToken(Resource):
                     try:
                         mail.send(msg)
                     except:
-                        return None
+                        return None, 400
+                    return None, 200
+                return None, 400
+            return None, 400
 
 
 class TokenVerification(Resource):
-    def post(self):
-        if request.data:
-            req = request.get_json(force=True)
-            if req['token']:
-                try:
-                    data = decode_token(req['token']);
-                except:
-                    return None
 
-                return data["identity"]
-        return None
+    @jwt_required
+    def get(self):
+        current_user = get_jwt_identity()
+
+        if not current_user:
+            return None, 401
+
+        return current_user, 200
+
+
+class CurrentDateTime(Resource):
+    def get(self):
+        return jsonify({ 'now': int(time.time()) })
 
 
 api.add_resource(EmailNotification, '/email')
 api.add_resource(EmailSendToken, '/token')
 api.add_resource(TokenVerification, '/verification')
+api.add_resource(CurrentDateTime, '/current-dt')
 
 if __name__ == '__main__':
     app.run(debug=True)
