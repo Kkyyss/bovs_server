4a1bcb45d89c96aaef98ff3e81849793057b92e3
Kkyyss <ongkangyi@gmail.com>
2018-11-08 07:32:08 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import json
 import os
 import time
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import date
 
 from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_restful import Resource, Api
@@ -12,6 +16,8 @@ from flask_jwt_extended import (
     decode_token, get_jwt_identity
 )
 
+scheduler = BackgroundScheduler()
+scheduler.start()
 
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
@@ -31,17 +37,65 @@ mail = Mail(app)
 api = Api(app)
 
 
+def sendEventStatusNotification(data):
+    with app.app_context():
+        emails = data['emails']
+        addr = data['addr']
+        election = data['election']
+        isStart = data['start']
+        creator = data['creator']
+        link = "http://localhost:3000/login/"
+        with mail.connect() as conn:
+            token = create_access_token(identity={ 'email': creator }, expires_delta=False)
+            link = link + 'organizer/' + creator + "/" + token + "/" + addr
+            msg = Message(
+                subject="[Blockchain Online Voting] Voting: " + election['title'] + (" started" if isStart else " closed"),
+                sender=app.config.get("MAIL_USERNAME"),
+                recipients=[creator],
+                html="Hi Organizer,<br/><br/><br/>" +
+                "The poll " + election['title'] + " has" + (" begin" if isStart else " over") + ".<br/>" +
+                "Please login to the <b>Blockchain Online Voting System</b> to " + ("vote" if isStart else "view results") + ":<br/>" +
+                '<a  href="' + link + '" target="_blank">' + link + '</a><br/><br/><br/>' +
+                "Sent by Blockchain Online Voting System"
+            )
+            try:
+                mail.send(msg)
+            except:
+                return None, 400
+            for email in emails:
+                if email == creator:
+                    continue
+                token = create_access_token(identity={ 'email': email }, expires_delta=False)
+                link = link + 'voter/' + email + "/" + token + "/" + addr
+                msg = Message(
+                    subject="[Blockchain Online Voting] Voting: " + election['title'] + (" started" if isStart else " closed"),
+                    sender=app.config.get("MAIL_USERNAME"),
+                    recipients=[email],
+                    html="Hi Voter,<br/><br/><br/>" +
+                    "The poll " + election['title'] + " has" + (" begin" if isStart else " over") + ".<br/>" +
+                    "Please login to the <b>Blockchain Online Voting System</b> to " + ("vote" if isStart else "view results") + ":<br/>" +
+                    '<a  href="' + link + '" target="_blank">' + link + '</a><br/><br/><br/>' +
+                    "Sent by Blockchain Online Voting System"
+                )
+                try:
+                    mail.send(msg)
+                except:
+                    return None, 400
+                return None, 200
+
+
 class EmailNotification(Resource):
-    @jwt_required
+    # @jwt_required
     def post(self):
         if request.data:
             req = request.get_json(force=True)
             emails = req['emails']
-            print(emails)
+            startDate = req['startDate']
+            isManual = req['isManual']
+            endDate = req['endDate']
             election = req['election']
             link = "http://localhost:3000/login/voter/"
             with mail.connect() as conn:
-                print('inside mail connected')
                 for email in emails:
                     token = create_access_token(identity={ 'email': email }, expires_delta=False)
                     link = link + email + "/" + token + "/" + req['addr']
@@ -59,7 +113,13 @@ class EmailNotification(Resource):
                         mail.send(msg)
                     except:
                         return None, 400
-                    return None, 200
+            if not election['startNow']:
+                scheduler.add_job(sendEventStatusNotification, 'date', run_date=startDate,
+                                  args=[{ 'emails': emails, 'creator': req['creator'], 'election': election, 'addr': req['addr'], 'start': True }])
+            if isManual:
+                scheduler.add_job(sendEventStatusNotification, 'date', run_date=startDate,
+                                  args=[{ 'emails': emails, 'creator': req['creator'], 'election': election, 'addr': req['addr'], 'start': False }])
+            return None, 200
 
 
 class EmailSendToken(Resource):
@@ -111,6 +171,8 @@ api.add_resource(EmailNotification, '/email')
 api.add_resource(EmailSendToken, '/token')
 api.add_resource(TokenVerification, '/verification')
 api.add_resource(CurrentDateTime, '/current-dt')
+
+atexit.register(lambda: scheduler.shutdown())
 
 if __name__ == '__main__':
     app.run(debug=True)
